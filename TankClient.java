43675026b38e498a4f359e5bce16e7c8eb2a02da
Gan <1354410847@qq.com>
2017-04-09 18:25:17 +0800

    Edition 0.42 : 使用线程完成重绘操作

--- TankClient.java
+++ TankClient.java
@@ -10,7 +10,7 @@ public class TankClient extends Frame{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	static int x = 50,y = 50;
 	
 	public void launchFrame(){
 		setBounds(200,200,600,800);
@@ -23,19 +23,44 @@ public class TankClient extends Frame{
 				System.exit(0);
 			}
 		});
+		new Thread(new PaintThread()).start();
 	}
 	
 	public void paint(Graphics g){
 		Color c = g.getColor();
 		g.setColor(Color.yellow);
-		g.fillOval(50, 50, 40, 40);
+		g.fillOval(x, y, 40, 40);
 		g.setColor(c);
+	}
+	
+	
+	private class PaintThread implements Runnable {
+		
+		public void run() {
+			int flag = 5;
+			while(true){
+				if(x >745 || y >545)
+					flag = -5;
+				if(x <20 || y < 10)
+					flag = 5 ;
+				try {
+					Thread.sleep(50);
+					x +=flag;
+					y +=flag;
+				} catch (Exception e) {
+					// TODO: handle exceptio
+				}
+				repaint();
+			}
+		}
+		
 	}
 	
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		TankClient tc = new TankClient();
 		tc.launchFrame();
+		
 	}
 
 }
