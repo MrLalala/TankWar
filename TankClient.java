a566ebf96f132c536832cb77e551aecec0adb810
Gan <1354410847@qq.com>
2017-04-09 20:32:50 +0800

    Edition 0.55 : 整理代码，设置一些常量，方便于修改

--- TankClient.java
+++ TankClient.java
@@ -12,13 +12,13 @@ public class TankClient extends Frame {
 	 */
 	private static final long serialVersionUID = 1L;
 	static int x = 50,y = 50;
-
+	static int h = 700,w = 800;
 	//实现双缓冲：
 	//新建一个图片对象
 	Image offScreen = null;
 
 	public void launchFrame() {
-		setBounds(200, 200, 600, 800);
+		setBounds(200, 200, h, w);
 		setLayout(null);
 		setBackground(Color.blue);
 		this.setResizable(false);
@@ -43,14 +43,14 @@ public class TankClient extends Frame {
 	public void update(Graphics g) {
 		//新建图片，该图片和主窗体一致
 		if(offScreen == null){
-			offScreen = this.createImage(800,600);
+			offScreen = this.createImage(h,w);
 		}
 		//新建画笔
 		Graphics goff = offScreen.getGraphics();
 		//获取颜色保护现场，该的各种参数应和主窗体一致
 		Color color = goff.getColor();
 		goff.setColor(Color.blue);
-		goff.fillRect(0, 0, 800, 600); 
+		goff.fillRect(0, 0,h, w); 
 		goff.setColor(color);
 		//调用绘图事件，将想要图案绘制到该图片上。
 		paint(goff);
@@ -79,7 +79,6 @@ public class TankClient extends Frame {
 		}
 	}
 	
-
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		TankClient tc = new TankClient();
