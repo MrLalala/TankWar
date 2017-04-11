6eb5eeb6464e1080dbb84c98c6d4b74453e94fa2
Gan <1354410847@qq.com>
2017-04-11 21:20:57 +0800

    图片版2.0：添加爆炸动画效果

--- Explode.java
+++ Explode.java
@@ -1,17 +1,35 @@
-import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 public class Explode {
 	//位置
 	int x,y;
 	//生死
 	private boolean live = true;
-	//爆炸半径
-	int[] dia = {4,7,18,26,35,49,35,26,18,7,4};
+//	//爆炸半径
+//	int[] dia = {4,7,18,26,35,49,35,26,18,7,4};
+	//同过 ToolKit勾取硬盘文件
+	private static Toolkit tk = Toolkit.getDefaultToolkit();
+	//新建图片数组
+	private static Image [] imgs = {
+			tk.getImage(Explode.class.getClassLoader().getResource("images/0.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/1.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/2.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/3.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/4.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/5.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/6.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/7.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/8.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/9.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/10.gif"))
+			
+	};
 	//执行步骤
 	int step = 0;
 	//爆炸颜色
-	private Color bloom = Color.orange;
+	//private Color bloom = Color.orange;
 	//拿到主窗体引用
 	private TankClient tc;
 	//构造方法
@@ -27,15 +45,12 @@ public class Explode {
 			tc.explodes.remove(this);
 			return;
 		}
-		if(step == dia.length){
+		if(step == imgs.length){
 			live =false;
 			step = 0;
 			return;
 		}
-		Color c = g.getColor();
-		g.setColor(bloom);
-		g.fillOval(x, y, dia[step], dia[step]);
-		g.setColor(c);
+		g.drawImage(imgs[step], x, y, null);
 		step++;
 	}
 }
